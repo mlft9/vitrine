34c0fd7cb7414eee7058ee81163fa2aaaf36a4c4
Unishadoweuh <unishadow@etik.com>
2025-04-05 15:21:28 +0200

    up GTM

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+
+
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { motion } from "framer-motion";
@@ -69,6 +71,13 @@ export default function Home() {
       </div>
 
       <Head>
+        <script>
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PXHHBV5K');
+        </script>
         <title>FrozenHost - Hébergement Cloud Performant</title>
         <link rel="icon" type="image/png" href="/logo.png" />
         <link rel="preload" href="/logo.png" as="image" />
